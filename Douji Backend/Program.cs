0d7bab84222c6f90c36101fb2e191d07555e95c0
Vojtěch Kursa <it.vojtechkursa@gmail.com>
2025-06-14 22:16:42 +0200

    Remove unnecessary configs and cleanup Backend's Program.cs

--- Douji Backend/Program.cs
+++ Douji Backend/Program.cs
@@ -6,71 +6,87 @@ namespace Douji.Backend
 {
 	public class Program
 	{
-		private static void AddCustomServices(WebApplicationBuilder builder) =>
-			builder.Services.AddSingleton<IDoujiInMemoryDb, DoujiInMemoryDb>();
-
-		private static void AddEndpointServices(WebApplicationBuilder builder)
-		{
-			builder.Services.AddControllers();
-			builder.Services.AddSignalR(options =>
-			{
-				if (builder.Environment.IsDevelopment())
-				{
-					options.EnableDetailedErrors = true;
-				}
-			});
-		}
-
-		private static void AddSecurityServices(WebApplicationBuilder builder)
-		{
-			builder.Services.AddCors(options =>
-			{
-				options.AddDefaultPolicy(policy =>
-				{
-					policy.WithOrigins("http://localhost:3000");
-					policy.WithMethods("GET", "POST", "PUT", "DELETE");
-					policy.AllowCredentials();
-					policy.AllowAnyHeader();
-				});
-			});
-		}
-
-		private static void AddSwagger(WebApplicationBuilder builder)
-		{
-			builder.Services.AddEndpointsApiExplorer();
-			builder.Services.AddSwaggerGen();
-		}
-
 		public static void Main(string[] args)
 		{
 			var builder = WebApplication.CreateBuilder(args);
 
-			AddEndpointServices(builder);
-			AddSecurityServices(builder);
-			AddSwagger(builder);
-			AddCustomServices(builder);
+			ServicesConfiguration.ConfigureServices(builder);
 
 			var app = builder.Build();
 
-			// Configure the HTTP request pipeline.
-			if (app.Environment.IsDevelopment())
-			{
-				app.UseSwagger();
-				app.UseSwaggerUI();
-			}
-
-			if (builder.Configuration.GetValue<bool?>("HttpsRedirect") ?? true)
-			{
-				app.UseHttpsRedirection();
-			}
-
-			app.UseAuthorization();
-
-			app.MapControllers();
-			app.MapHub<RoomHub>("/hub/room");
-			app.UseCors();
+			MiddlewareConfiguration.ConfigureMiddleware(app);
 
 			app.Run();
+		}
+
+		private static class ServicesConfiguration
+		{
+			public static void ConfigureServices(WebApplicationBuilder builder)
+			{
+				AddEndpointServices(builder);
+				AddSecurityServices(builder);
+				AddSwagger(builder);
+				AddCustomServices(builder);
+			}
+
+			private static void AddCustomServices(WebApplicationBuilder builder) =>
+			builder.Services.AddSingleton<IDoujiInMemoryDb, DoujiInMemoryDb>();
+
+			private static void AddEndpointServices(WebApplicationBuilder builder)
+			{
+				builder.Services.AddControllers();
+				builder.Services.AddSignalR(options =>
+				{
+					if (builder.Environment.IsDevelopment())
+					{
+						options.EnableDetailedErrors = true;
+					}
+				});
+			}
+
+			private static void AddSecurityServices(WebApplicationBuilder builder)
+			{
+				builder.Services.AddCors(options =>
+				{
+					options.AddDefaultPolicy(policy =>
+					{
+						policy.WithOrigins("http://localhost:3000");
+						policy.WithMethods("GET", "POST", "PUT", "DELETE");
+						policy.AllowCredentials();
+						policy.AllowAnyHeader();
+					});
+				});
+			}
+
+			private static void AddSwagger(WebApplicationBuilder builder)
+			{
+				builder.Services.AddEndpointsApiExplorer();
+				builder.Services.AddSwaggerGen();
+			}
+		}
+
+		private static class MiddlewareConfiguration
+		{
+			public static void ConfigureMiddleware(WebApplication app)
+			{
+				if (app.Environment.IsDevelopment())
+				{
+					app.UseSwagger();
+					app.UseSwaggerUI();
+				}
+
+				if (app.Configuration.GetValue<bool?>("HttpsRedirect") ?? true)
+				{
+					app.UseHttpsRedirection();
+				}
+
+				app.UseAuthorization();
+
+				app.MapControllers();
+				app.MapHub<RoomHub>("/hub/room");
+
+				app.UseCors();
+			}
 		}
 	}
 }
